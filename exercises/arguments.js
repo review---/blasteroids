b009d712d927d6cf514f4cb9453d42454c6128b2
Nick Willett-Jeffries & Brian Deane <nick+brian@appacademy.io>
2013-11-27 09:48:45 -0500

    Finish curry exercises

--- exercises/arguments.js
+++ exercises/arguments.js
@@ -1,3 +1,5 @@
+var __ = require("underscore");
+
 function sum() {
   var sum = 0
 
@@ -26,3 +28,52 @@ Function.prototype.myBind = function(obj) {
 // var myFunction = function(a,b,c) { console.log(this.name + a + b + c); };
 // var myBoundFunction = myFunction.myBind(cat, 1, 2);
 // myBoundFunction(3);
+
+function curriedSum(numArgs) {
+  var numbers = [];
+
+  function _curriedSum(num1) {
+    numbers.push(num1);
+
+    if (numbers.length === numArgs) {
+      return __.inject(numbers, function (sum, num2) {
+        return sum + num2;
+      });
+    } else {
+      return _curriedSum;
+    };
+  };
+
+  return _curriedSum;
+};
+//
+// var sum = curriedSum(4);
+// console.log(sum(5)(1)(8)(1));
+
+Function.prototype.curry = function(numArgs) {
+  var args = [];
+  var self = this;
+
+  function _curriedVals(arg) {
+    args.push(arg);
+
+    if (args.length === numArgs) {
+      return self.apply(self, args);
+    } else {
+      return _curriedVals;
+    };
+  };
+
+  return _curriedVals;
+};
+
+// function sumThree(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
+// var f1 = sumThree.curry(3);
+// var f2 = f1(4);
+// var f3 = f2(20);
+// var result = f3(3);
+//
+// console.log(sumThree.curry(3)(4)(20)(3));
+// console.log(result);
