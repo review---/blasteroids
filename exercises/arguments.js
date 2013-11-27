682f0de95362499d8aed88d69bc8971d74669b46
Nick Willett-Jeffries & Brian Deane <nick+brian@appacademy.io>
2013-11-27 09:15:48 -0500

    Finished myBind magic

--- exercises/arguments.js
+++ exercises/arguments.js
@@ -8,5 +8,21 @@ function sum() {
   return sum;
 };
 
-console.log(sum(1, 2, 3, 4) == 10);
-console.log(sum(1, 2, 3, 4, 5) == 15);
+// console.log(sum(1, 2, 3, 4) == 10);
+// console.log(sum(1, 2, 3, 4, 5) == 15);
+
+Function.prototype.myBind = function(obj) {
+  var args1 = Array.prototype.slice.call(arguments).slice(1);
+  var self = this;
+
+  return function() {
+    var args2 = Array.prototype.slice.call(arguments);
+    var args = args1.concat(args2);
+    self.apply(obj, args);
+  };
+};
+
+// var cat = { name: "Sennacy" };
+// var myFunction = function(a,b,c) { console.log(this.name + a + b + c); };
+// var myBoundFunction = myFunction.myBind(cat, 1, 2);
+// myBoundFunction(3);
